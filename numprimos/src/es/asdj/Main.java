62e6a9cfac6af901c56cc8e8a213d93d8819d17b
al036991 <al036991@ieslaencanta.com>
2022-01-24 10:40:38 +0100

    inicio primos

---
+++ numprimos/src/es/asdj/Main.java
@@ -0,0 +1,20 @@
+package es.asdj;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int numero,contador;
+    Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Escribe un numero para comprobar si es primo: ");
+        numero = entrada.nextInt();
+
+    for (int i = 0; i < numero ; i++);
+
+        if (contador > 2){
+
+        }
+    }
+}

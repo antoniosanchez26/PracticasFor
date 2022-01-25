a842654bd9691550b1c78130449aa921af956742
Antonio Sánchez <al036991@ieslaencanta.com>
2022-01-25 12:33:11 +0100

    números primos terminados.

--- numprimos/src/es/asdj/Main.java
+++ numprimos/src/es/asdj/Main.java
@@ -1,20 +1,30 @@
 package es.asdj;
 
+
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-    int numero,contador;
-    Scanner entrada = new Scanner(System.in);
+        int numero, contador = 0;
+        Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Escribe un numero para comprobar si es primo: ");
+        System.out.print("Escribe un número para comprobar si es primo: ");
         numero = entrada.nextInt();
 
-    for (int i = 0; i < numero ; i++);
+        for (int i = 1; i <= numero; i++) {
+            // Comprobar si la i es divisor del numero.
+            // Cuando sea divisor del numero sumo uno a contador.
+            // numero % i == 0
+            if (numero % i == 0) {
+                contador++;
+            }
 
-        if (contador > 2){
-
+        }
+        if  (contador <= 2){
+            System.out.print("El número  es primo");
+        } else {
+            System.out.print("el número no es primo");
         }
     }
 }
+
